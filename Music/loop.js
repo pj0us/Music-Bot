5a5271350a4f3f240fca19bff91b552302905ce7
Do Nguyen Trung Hieu <87639535+PiousGaming@users.noreply.github.com>
2021-10-05 16:09:58 +0700

    Update loop.js

--- Music/loop.js
+++ Music/loop.js
@@ -1,7 +1,7 @@
 ////////////////////////////
 //////CONFIG LOAD///////////
 ////////////////////////////
-const { canModifyQueue } = require("../util/MilratoUtil");
+const { canModifyQueue } = require("../util/Util");
 const { MessageEmbed } = require("discord.js");
 const { attentionembed } = require("../util/attentionembed"); 
 const { prf } = require(`../config.json`);
